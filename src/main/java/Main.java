094cdc22bd9d0d4021bf91dab7a74f2f68ff5a3a
asya afanasyeva <afanasyeva656@gmail.com>
2021-10-01 15:12:43 +0200

    Completed 1st exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("voyna.txt");
+        Parser parser = new Parser();
+        ArrayList<String> list = null;
+        try {
+            list = parser.parse(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int wordCount = getWordCount("страдани[иеяй].?", list);
+        System.out.println("Число вхождений слова 'страдание': " + wordCount);
+
+    }
+
+
+    private static int getWordCount(String wordPattern, ArrayList<String> list) {
+        int wordCount = 0;
+        if (list != null) {
+            Pattern pattern = Pattern.compile(wordPattern, Pattern.CASE_INSENSITIVE);
+
+            for (String s : list) {
+                Matcher matcher = pattern.matcher(s);
+                while (matcher.find()) {
+                    wordCount++;
+                }
+            }
+        }
+        return wordCount;
+    }
+}
